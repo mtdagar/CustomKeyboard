abe2a5b5c038d42c5542991bfcf4cf473d4390c6
Meet Dagar <mtdagar@gmail.com>
2021-07-28 00:48:47 +0530

    first functional commit

--- app/src/main/java/com/example/customkeyboard/CustomIME.java
+++ app/src/main/java/com/example/customkeyboard/CustomIME.java
@@ -2,12 +2,140 @@ package com.example.customkeyboard;
 
 
 import android.inputmethodservice.InputMethodService;
+import android.util.Log;
+import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.view.inputmethod.InputConnection;
 
+import com.example.customkeyboard.views.CandidateView;
 import com.example.customkeyboard.views.KeyboardView;
 
 /**
  * Input method implementation for Qwerty keyboard.
  */
 
-public class CustomIME extends InputMethodService implements KeyboardView.KeyboardListener {
+public class CustomIME extends InputMethodService implements KeyboardView.KeyboardListener, CandidateView.CandidateListener {
+    private final static String TAG = "Playground";
+    private StringBuilder currentWord;
+    private KeyboardView keyboardView;
+    private CandidateView candidateLayout;
+    private InputConnection inputConnection;
+    //private IterativeWordSuggestion iterativeWordSuggestion;
+    private static CustomIME instance;
+
+    public void setSize(KeyboardView.Size size) {
+        if (keyboardView != null)
+            keyboardView.setSize(size);
+    }
+
+    public static CustomIME getInstance() {
+        return instance;
+    }
+
+    @Override
+    public void onCreate() {
+        Log.d(TAG, "onCreate called");
+        instance = this;
+        currentWord = new StringBuilder();
+        //iterativeWordSuggestion = new IterativeWordSuggestion(getApplicationContext());
+        super.onCreate();
+    }
+
+    @Override
+    public void onInitializeInterface() {
+        Log.d(TAG, "onInitialiseInterface called");
+    }
+
+
+    @Override
+    public View onCreateInputView() {
+        Log.d(TAG, "onCreateInputView called");
+        keyboardView = new KeyboardView(getApplicationContext());
+        keyboardView.setKeyboardListener(this);
+        setCandidatesViewShown(true);
+        return keyboardView;
+    }
+
+    @Override
+    public View onCreateCandidatesView() {
+        Log.d(TAG, "onCreateCandidateView called");
+        candidateLayout = new CandidateView(getApplicationContext());
+        candidateLayout.setWordSelectedListener(this);
+        return candidateLayout;
+    }
+
+    @Override
+    public void onStartInputView(EditorInfo info, boolean restarting) {
+        Log.d(TAG, "onStartInputView"+ KeyboardView.Size.LARGE);
+        keyboardView.setSize(KeyboardView.Size.LARGE);
+        inputConnection = getCurrentInputConnection();
+        keyboardView.reset();
+    }
+
+    @Override
+    public void onFinishInput() {
+        Log.d(TAG, "onFinishInput called");
+        super.onFinishInput();
+    }
+
+    @Override
+    public void onDestroy() {
+        //iterativeWordSuggestion.close();
+        Log.d(TAG, "onDestroy called");
+        super.onDestroy();
+    }
+
+    @Override
+    public void onCharacterKey(char a) {
+        inputConnection.commitText(String.valueOf(a), 1);
+        currentWord.append(a);
+        //candidateLayout.setSuggestion(iterativeWordSuggestion.getSuggestion(a));
+    }
+
+    @Override
+    public void onBackspace() {
+        inputConnection.deleteSurroundingText(1, 0);
+        candidateLayout.setSuggestion(null);
+        //iterativeWordSuggestion.reset();
+        try {
+            currentWord.deleteCharAt(currentWord.length() - 1);
+        } catch (StringIndexOutOfBoundsException e) {
+            currentWord = new StringBuilder();
+        }
+    }
+
+    @Override
+    public void onComputeInsets(InputMethodService.Insets outInsets) {
+        super.onComputeInsets(outInsets);
+        if (!isFullscreenMode()) {
+            outInsets.contentTopInsets = outInsets.visibleTopInsets;
+        }
+    }
+
+    @Override
+    public void onSpace() {
+        inputConnection.commitText(" ", 1);
+        candidateLayout.setSuggestion(null);
+        //iterativeWordSuggestion.reset();
+        currentWord = new StringBuilder();
+    }
+
+    @Override
+    public void onCap() {
+        //TODO
+    }
+
+    @Override
+    public void onReturn() {
+        inputConnection.commitText("\n", 1);
+        currentWord = new StringBuilder();
+    }
+
+    @Override
+    public void onSelectSuggestion(String s) {
+        inputConnection.commitText(s.substring(currentWord.length()) + " ", 1);
+        //iterativeWordSuggestion.reset();
+        //iterativeWordSuggestion.updateFrequency(s);
+        currentWord = new StringBuilder();
+    }
 }
--- app/src/main/java/com/example/customkeyboard/activity/SettingsActivity.java
+++ app/src/main/java/com/example/customkeyboard/activity/SettingsActivity.java
@@ -2,15 +2,86 @@ package com.example.customkeyboard.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.provider.Settings;
+import android.view.inputmethod.InputMethodInfo;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioGroup;
+import android.widget.Toast;
 
+import com.example.customkeyboard.CustomIME;
 import com.example.customkeyboard.R;
+import com.example.customkeyboard.views.KeyboardView;
+
+import java.security.Key;
+import java.util.List;
 
 public class SettingsActivity extends AppCompatActivity {
 
+    EditText editTextTest;
+    Button buttonEnableKeyboard;
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+
+        editTextTest = findViewById(R.id.etTest);
+        buttonEnableKeyboard = findViewById(R.id.button_enable_keyboard);
+
+        buttonEnableKeyboard.setOnClickListener(v -> enableKeyboard());
+    }
+
+//    private void setupSize() {
+//        RadioGroup group = findViewById(R.id.size);
+//
+//        group.check(R.id.large);
+//
+//        group.setOnCheckedChangeListener((group1, checkedId) -> {
+//            KeyboardLayout.Size size = null;
+//            switch (checkedId) {
+//                case R.id.small:
+//                    size = KeyboardLayout.Size.SMALL;
+//                    break;
+//                case R.id.large:
+//                    size = KeyboardLayout.Size.LARGE;
+//                    break;
+//                case R.id.medium:
+//                    size = KeyboardLayout.Size.MEDIUM;
+//            }
+////            SPManager.setSize(getApplicationContext(), size);
+//            changeSize(size);
+//        });
+//    }
+
+    private void changeSize(KeyboardView.Size size) {
+        CustomIME ime = CustomIME.getInstance();
+        if (ime != null)
+            ime.setSize(size);
+    }
+
+    void enableKeyboard() {
+        InputMethodManager imeManager = (InputMethodManager) getSystemService(INPUT_METHOD_SERVICE);
+        boolean enabled = false;
+        List<InputMethodInfo> listOfKeyboards = imeManager.getEnabledInputMethodList();
+        for (InputMethodInfo i : listOfKeyboards) {
+            if (i.getPackageName().compareTo(getPackageName()) == 0) {
+                if (i.getId().equals(Settings.Secure.getString(getContentResolver(), Settings.Secure.DEFAULT_INPUT_METHOD))) {
+                    Toast.makeText(SettingsActivity.this, "Keyboard is enabled", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                enabled = true;
+                break;
+            }
+        }
+        if (!enabled) {
+            Intent enableIntent = new Intent(Settings.ACTION_INPUT_METHOD_SETTINGS);
+            enableIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            startActivity(enableIntent);
+        } else
+            imeManager.showInputMethodPicker();
     }
 }
